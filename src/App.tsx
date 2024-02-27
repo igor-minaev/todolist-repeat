b047fd583ef62c1f7dea3f3ac29d61ece706126c
Igor Minayeu <igor.minayeu@gmail.com>
2024-02-27 20:39:39 +0300

    add filtration for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './components/Todolist';
 
+type CategoryType = 'Frontend' | 'Backend'
+export type TaskType = {
+    id: string
+    category: CategoryType
+    title: string
+    isDone: boolean
+}
+export type FilterType = 'all' | 'active' | 'completed'
+
 function App() {
+    const todolistTitle = 'What to learn'
+    const [tasks, setTasks] = useState<TaskType[]>([
+        {id: crypto.randomUUID(), category: 'Frontend', title: 'HTML', isDone: true},
+        {id: crypto.randomUUID(), category: 'Backend', title: 'NODE JS', isDone: true},
+        {id: crypto.randomUUID(), category: 'Frontend', title: 'CSS', isDone: true},
+        {id: crypto.randomUUID(), category: 'Backend', title: 'DOCKER', isDone: false},
+        {id: crypto.randomUUID(), category: 'Frontend', title: 'REACT', isDone: false},
+        {id: crypto.randomUUID(), category: 'Frontend', title: 'REDUX', isDone: false}
+    ])
+    const [filter, setFilter] = useState<FilterType>('all')
+
+    const removeTask = (taskId: string) => setTasks(tasks.filter(t => t.id !== taskId))
+    const changeFilter = (newFilterValue: FilterType) => setFilter(newFilterValue)
+
+    const getFilteredTasks = (allTask: TaskType[], filter: FilterType): TaskType[] => {
+        switch (filter) {
+            case 'active':
+                return allTask.filter(t => !t.isDone)
+            case 'completed':
+                return allTask.filter(t => t.isDone)
+            default:
+                return allTask
+        }
+    }
+    const filteredTasks = getFilteredTasks(tasks, filter)
+
     return (
         <div className="App">
-            <Todolist/>
+            <Todolist
+                title={todolistTitle}
+                tasks={filteredTasks}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+            />
         </div>
     );
 }
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {FilterType, TaskType} from '../App';
 
-export const Todolist = () => {
+
+type TodolistPropsType = {
+    title: string
+    tasks: TaskType[]
+    removeTask: (taskId: string) => void
+    changeFilter: (newFilterValue: FilterType) => void
+}
+
+export const Todolist: FC<TodolistPropsType> = (
+    {
+        title, tasks, removeTask, changeFilter
+    }) => {
+    const mappedTasks: JSX.Element[] = tasks.map(task => {
+        const onClickHandler = () => removeTask(task.id)
+        return (
+            <li key={task.id}>
+                <input type="checkbox" checked={task.isDone}/>
+                <span>{task.title} - </span>
+                <span>{task.category} </span>
+                <button onClick={onClickHandler}>x</button>
+            </li>
+        )
+    })
+    const onClickAllFilter = () => changeFilter('all')
+    const onClickActiveFilter = () => changeFilter('active')
+    const onClickCompletedFilter = () => changeFilter('completed')
+
+    const tasksForRender: JSX.Element = tasks.length
+        ? <ul>{mappedTasks}</ul>
+        : <span>Your task list is empty!</span>
     return (
-        <div className='todolist'>
-            <h3>What to learn</h3>
-            <div>
+        <div className="todolist">
+            <h3>{title}</h3>
+            <div className="input">
                 <input/>
                 <button>+</button>
             </div>
-            <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
-            </ul>
-            <div className='buttons'>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+            {tasksForRender}
+            <div className="buttons">
+                <button onClick={onClickAllFilter}>All</button>
+                <button onClick={onClickActiveFilter}>Active</button>
+                <button onClick={onClickCompletedFilter}>Completed</button>
             </div>
         </div>
     );
