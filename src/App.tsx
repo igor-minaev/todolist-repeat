3223b226af92bf14fb38e6002d07daebf99025c1
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-11 16:57:42 +0300

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,20 @@
 import React, {useState} from 'react';
 import './App.css';
-import {TodoList} from './components/TodoList';
+import {Todolist} from './components/Todolist';
 
-export type TaskType = {
-    id: string
-    title: string
-    isDone: boolean
-}
-export type FilterType = 'All' | 'Active' | 'Completed'
 
 function App() {
-    const todoListTitle = 'What to learn'
-    const [tasks, setTasks] = useState<TaskType[]>([
-        {id: crypto.randomUUID(), title: 'HTML', isDone: true},
-        {id: crypto.randomUUID(), title: 'CSS', isDone: true},
-        {id: crypto.randomUUID(), title: 'JS', isDone: true},
-        {id: crypto.randomUUID(), title: 'REACT', isDone: false},
-        {id: crypto.randomUUID(), title: 'REDUX', isDone: false},
-        {id: crypto.randomUUID(), title: 'GIT', isDone: true},
-        {id: crypto.randomUUID(), title: 'RTK', isDone: false}
+    const todolistTitle = 'What to learn'
+    const [tasks, setTasks] = useState([
+        {id: crypto.randomUUID(), title: 'HTML', direction: 'Frontend', isDone: true},
+        {id: crypto.randomUUID(), title: 'Node JS', direction: 'Backend', isDone: false},
+        {id: crypto.randomUUID(), title: 'CSS', direction: 'Frontend', isDone: true},
+        {id: crypto.randomUUID(), title: 'REACT', direction: 'Frontend', isDone: false},
+        {id: crypto.randomUUID(), title: 'SQL', direction: 'Backend', isDone: false}
     ])
-    const [filter, setFilter] = useState<FilterType>('All')
-
-    const addTask = (title: string) => {
-        const newTask: TaskType = {id: crypto.randomUUID(), title, isDone: false}
-        setTasks([newTask, ...tasks])
-    }
-
-    const changeTaskStatus = (taskId: string, newTaskStatus: boolean) => {
-        setTasks(tasks.map(t => t.id === taskId ? {...t, isDone: newTaskStatus} : t))
-    }
-
-    const removeTask = (taskId: string) => setTasks(tasks.filter(t => t.id !== taskId))
-
-    const changeFilter = (newFilterValue: FilterType) => setFilter(newFilterValue)
-
-    const getFilteredTasks = (tasks: TaskType[], filter: FilterType): TaskType[] => {
-        switch (filter) {
-            case 'Active':
-                return tasks.filter(t => !t.isDone)
-            case 'Completed':
-                return tasks.filter(t => t.isDone)
-            default:
-                return tasks
-        }
-    }
-    const filteredTasks = getFilteredTasks(tasks, filter)
     return (
         <div className="App">
-            <TodoList
-                title={todoListTitle}
-                tasks={filteredTasks}
-                filter={filter}
-                removeTask={removeTask}
-                changeFilter={changeFilter}
-                addTask={addTask}
-                changeTaskStatus={changeTaskStatus}
-            />
+            <Todolist/>
         </div>
     );
 }
