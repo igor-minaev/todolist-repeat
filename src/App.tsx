40dd794040458e59b43964aa1754819c1ff7918f
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-11 17:24:30 +0300

    add function removeTask

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,31 @@ import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './components/Todolist';
 
+export type DirectionType = 'Frontend' | 'Backend'
+
+export type TaskType = {
+    id: string
+    title: string
+    direction: DirectionType
+    isDone: boolean
+}
 
 function App() {
     const todolistTitle = 'What to learn'
-    const [tasks, setTasks] = useState([
+    const [tasks, setTasks] = useState<TaskType[]>([
         {id: crypto.randomUUID(), title: 'HTML', direction: 'Frontend', isDone: true},
         {id: crypto.randomUUID(), title: 'Node JS', direction: 'Backend', isDone: false},
         {id: crypto.randomUUID(), title: 'CSS', direction: 'Frontend', isDone: true},
         {id: crypto.randomUUID(), title: 'REACT', direction: 'Frontend', isDone: false},
         {id: crypto.randomUUID(), title: 'SQL', direction: 'Backend', isDone: false}
     ])
+    const removeTask = (taskId: string) => setTasks(tasks.filter(t => t.id !== taskId))
     return (
         <div className="App">
-            <Todolist/>
+            <Todolist
+                todolistTitle={todolistTitle}
+                tasks={tasks}
+                removeTask={removeTask}/>
         </div>
     );
 }
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import {TaskType} from '../App';
 
-export const Todolist = () => {
+type TodolistPropsType = {
+    todolistTitle: string
+    tasks: TaskType[]
+    removeTask: (taskId: string) => void
+}
+
+export const Todolist: React.FC<TodolistPropsType> = (props) => {
+    const {todolistTitle, tasks, removeTask, ...restProps} = props
+    const listItems: JSX.Element[] = tasks.map(task => {
+        const removeTaskHandler = () => removeTask(task.id)
+        return (
+            <li key={task.id}>
+                <input type="checkbox" checked={task.isDone}/>
+                <span>{task.title}</span>
+                <span>- {task.direction}</span>
+                <button onClick={removeTaskHandler}>x</button>
+            </li>
+        )
+    })
+    const tasksForTodolist: JSX.Element = tasks.length
+        ? <ul>{listItems}</ul>
+        : <p>Your todolist is empty!</p>
     return (
-        <div className='todolist'>
-            <h3 className='title'>sdfsdfsdfsdf</h3>
+        <div className="todolist">
+            <h3 className="title">{todolistTitle}</h3>
             <div>
                 <input/>
                 <select>
@@ -13,8 +35,8 @@ export const Todolist = () => {
                 </select>
                 <button>+</button>
             </div>
-            <ul></ul>
-            <div className='buttons'>
+            {tasksForTodolist}
+            <div className="buttons">
                 <button>All</button>
                 <button>Active</button>
                 <button>Completed</button>
