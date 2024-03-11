7814e48336d0a6357ece795f2ad472d4b6b4507d
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-11 17:07:49 +0300

    add styles for todolist

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 export const Todolist = () => {
     return (
-        <div>
-            sdfgdfgdfgdrfgdf
+        <div className='todolist'>
+            <h3 className='title'>sdfsdfsdfsdf</h3>
+            <div>
+                <input/>
+                <select>
+                    <option value="-">Choose direction</option>
+                    <option value="Frontend">Frontend</option>
+                    <option value="Backend">Backend</option>
+                </select>
+                <button>+</button>
+            </div>
+            <ul></ul>
+            <div className='buttons'>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
         </div>
     );
 };
