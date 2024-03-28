7284dce57629fe327fcfa57f984fd1d1841f8370
Igor Minayeu <igor.minayeu@gmail.com>
2024-03-28 16:30:56 +0300

    add filtration for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from './components/Todolist';
 
+export type FilterType = 'All' | 'Active' | 'Completed'
 
 function App() {
     const todolistTitle = 'What to learn'
@@ -12,13 +13,17 @@ function App() {
         {id: crypto.randomUUID(), title: 'JS', isDone: true},
         {id: crypto.randomUUID(), title: 'REDUX', isDone: false},
     ])
+    const [filter, setFilter] = useState<FilterType>('All')
 
     const removeTask = (taskId: string) => setTasks(tasks.filter(t => t.id !== taskId))
+    const changeFilter = (filter: FilterType) => setFilter(filter)
     return <div className="App">
         <Todolist
+            filter={filter}
             title={todolistTitle}
             tasks={tasks}
-            removeTask={removeTask}/>
+            removeTask={removeTask}
+            changeFilter={changeFilter}/>
     </div>
 }
 
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Task} from './Task';
 import {Button} from './Button';
+import {FilterType} from '../App';
 
 type TodolistType = {
+    filter: FilterType
     title: string
     tasks: TaskType[]
     removeTask: (taskId: string) => void
+    changeFilter: (filter: FilterType) => void
 }
 
 export type TaskType = {
@@ -15,14 +18,31 @@ export type TaskType = {
 }
 
 export const Todolist: React.FC<TodolistType> = props => {
-    const {title, tasks, removeTask, ...restProps} = props
+    const {filter, title, tasks, removeTask, changeFilter, ...restProps} = props
 
-    const mappedTasks: JSX.Element[] = tasks.map(task => {
+    const getFilteredTasks = (tasks: TaskType[], filter: FilterType): TaskType[] => {
+        switch (filter) {
+            case 'Active':
+                return tasks.filter(t => !t.isDone)
+            case 'Completed':
+                return tasks.filter(t => t.isDone)
+            default:
+                return tasks
+
+        }
+    }
+    const tasksForTodolist = getFilteredTasks(tasks, filter)
+
+    const mappedTasks: JSX.Element[] = tasksForTodolist.map(task => {
         return <Task key={task.id} {...task} removeTask={removeTask}/>
     })
     const tasksForRender: JSX.Element = tasks.length
         ? <ul className="list">{mappedTasks}</ul>
         : <p>Your todolist is empty!</p>
+
+    const changeFilterAllHandler = () => changeFilter('All')
+    const changeFilterActiveHandler = () => changeFilter('Active')
+    const changeFilterCompletedHandler = () => changeFilter('Completed')
 
     return (
         <div className="todolist">
@@ -34,12 +54,9 @@ export const Todolist: React.FC<TodolistType> = props => {
             </div>
             {tasksForRender}
             <div>
-                <Button name="All" onClick={() => {
-                }}/>
-                <Button name="Active" onClick={() => {
-                }}/>
-                <Button name="Completed" onClick={() => {
-                }}/>
+                <Button name="All" onClick={changeFilterAllHandler}/>
+                <Button name="Active" onClick={changeFilterActiveHandler}/>
+                <Button name="Completed" onClick={changeFilterCompletedHandler}/>
             </div>
         </div>
     );
