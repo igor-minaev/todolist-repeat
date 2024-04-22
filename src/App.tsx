ebb7875d366bf9cd4b2f398f35b7916664cf791c
Igor Minayeu <igor.minayeu@gmail.com>
2024-04-22 14:23:11 +0300

    add tasksReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 import {v1} from 'uuid';
 import {Todolist} from './components/Todolist';
@@ -6,6 +6,7 @@ import {AddItemForm} from './components/AddItemForm';
 import {AppBarLogin} from './components/AppBarLogin';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, taskReducer} from './reducers/taskReducer';
 
 export type TaskType = {
     id: string
@@ -30,7 +31,7 @@ function App() {
         {id: todolistId_2, title: 'What to buy', filter: 'all'},
     ])
 
-    const [tasks, setTasks] = useState<TasksStateType>({
+    const [tasks, dispatchTasks] = useReducer(taskReducer, {
         [todolistId_1]: [
             {id: v1(), title: 'HTML', isDone: true},
             {id: v1(), title: 'CSS', isDone: true},
@@ -45,21 +46,17 @@ function App() {
         ]
     })
 
-
     const removeTask = (todolistId: string, taskId: string) => {
-        setTasks({
-            ...tasks, [todolistId]: tasks[todolistId].filter(t => t.id !== taskId)
-        })
+        dispatchTasks(removeTaskAC(todolistId,taskId))
     }
     const addTask = (todolistId: string, title: string) => {
-        const newTask: TaskType = {id: v1(), title, isDone: false}
-        setTasks({...tasks, [todolistId]: [newTask, ...tasks[todolistId]]})
+       dispatchTasks(addTaskAC(todolistId,title))
     }
     const changeTaskStatus = (todolistId: string, taskId: string, isDone: boolean) => {
-        setTasks({...tasks, [todolistId]: tasks[todolistId].map(t => t.id === taskId ? {...t, isDone} : t)})
+      dispatchTasks(changeTaskStatusAC(todolistId,taskId,isDone))
     }
     const changeTaskTitle = (todolistId: string, taskId: string, title: string) => {
-        setTasks({...tasks, [todolistId]: tasks[todolistId].map(t => t.id === taskId ? {...t, title} : t)})
+        dispatchTasks(changeTaskTitleAC(todolistId,taskId,title))
     }
 
     const removeTodolist = (todolistId: string) => {
@@ -82,19 +79,19 @@ function App() {
     const mappedTodolists: JSX.Element[] = todolists.map(t => {
         return (
             <Grid item>
-                    <Todolist
-                        key={t.id}
-                        todolistId={t.id}
-                        filter={t.filter}
-                        title={t.title}
-                        tasks={tasks[t.id]}
-                        removeTask={removeTask}
-                        changeTodolistFilter={changeTodolistFilter}
-                        addTask={addTask}
-                        changeTaskStatus={changeTaskStatus}
-                        removeTodolist={removeTodolist}
-                        changeTodolistTitle={changeTodolistTitle}
-                        changeTaskTitle={changeTaskTitle}/>
+                <Todolist
+                    key={t.id}
+                    todolistId={t.id}
+                    filter={t.filter}
+                    title={t.title}
+                    tasks={tasks[t.id]}
+                    removeTask={removeTask}
+                    changeTodolistFilter={changeTodolistFilter}
+                    addTask={addTask}
+                    changeTaskStatus={changeTaskStatus}
+                    removeTodolist={removeTodolist}
+                    changeTodolistTitle={changeTodolistTitle}
+                    changeTaskTitle={changeTaskTitle}/>
             </Grid>
         )
     })
