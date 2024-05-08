079ac4a82764bd5ea1ddd135070d8526e526ae2d
Igor Minayeu <igor.minayeu@gmail.com>
2024-05-08 17:50:10 +0300

    add props for todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './components/Todolist';
 
+export type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
+
 
 function App() {
+    const todolistTitle = 'What to learn'
+
+    const [tasks, setTasks] = useState<TaskType[]>([
+        {id: crypto.randomUUID(), title: 'HTML', isDone: true},
+        {id: crypto.randomUUID(), title: 'CSS', isDone: true},
+        {id: crypto.randomUUID(), title: 'JS', isDone: true},
+        {id: crypto.randomUUID(), title: 'REACT', isDone: false},
+        {id: crypto.randomUUID(), title: 'REDUX', isDone: false}
+    ])
 
     return (
         <div className="App">
-           <Todolist/>
-           <Todolist/>
-           <Todolist/>
-           <Todolist/>
-           <Todolist/>
-           <Todolist/>
-           <Todolist/>
-           <Todolist/>
-           <Todolist/>
+            <Todolist title={todolistTitle} tasks={tasks}/>
         </div>
     )
 }
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import {TaskType} from '../App';
 
-export const Todolist = () => {
+type TodolistPropsType = {
+    title: string
+    tasks: TaskType[]
+}
+
+export const Todolist: React.FC<TodolistPropsType> = (props) => {
+    const {title, tasks, ...restProps} = props
+    const listItems: JSX.Element[] = tasks.map(t => {
+        return (
+            <li key={t.id}>
+                <input type="checkbox" checked={t.isDone}/>
+                <span>{t.title}</span>
+            </li>
+        )
+    })
+    const mappedTasks: JSX.Element = tasks.length
+        ? <ul>{listItems}</ul>
+        : <p>Your todolist is empty!</p>
     return (
-        <div className='todolist'>
-            <h2>Todolist</h2>
+        <div className="todolist">
+            <h2>{title}</h2>
             <div>
                 <input/>
                 <button>+</button>
             </div>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            {mappedTasks}
             <div>
                 <button>All</button>
                 <button>Active</button>
