b6da93d6a8717b2681f2ec7fbb7de22d86b1a2d8
Igor Minayeu <igor.minayeu@gmail.com>
2024-05-08 21:14:18 +0300

    change state

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,40 @@ export type TaskType = {
 }
 
 export type FilterType = 'all' | 'active' | 'completed'
+export type TodolistType = {
+    id: string
+    title: string
+    filter: FilterType
+}
+export type TasksStateType = {
+    [key: string]: TaskType[]
+}
 
 function App() {
-    const todolistTitle = 'What to learn'
-
-    const [tasks, setTasks] = useState<TaskType[]>([
-        {id: crypto.randomUUID(), title: 'HTML', isDone: true},
-        {id: crypto.randomUUID(), title: 'CSS', isDone: true},
-        {id: crypto.randomUUID(), title: 'JS', isDone: true},
-        {id: crypto.randomUUID(), title: 'REACT', isDone: false},
-        {id: crypto.randomUUID(), title: 'REDUX', isDone: false}
+    const todolistId_1 = crypto.randomUUID()
+    const todolistId_2 = crypto.randomUUID()
+    const [todolists, setTodolists] = useState<TodolistType[]>([
+        {id: todolistId_1, title: 'What to learn', filter: 'all'},
+        {id: todolistId_2, title: 'What to learn2', filter: 'all'}
     ])
+
+    const [tasks, setTasks] = useState<TasksStateType>({
+        [todolistId_1]: [
+            {id: crypto.randomUUID(), title: 'HTML', isDone: true},
+            {id: crypto.randomUUID(), title: 'CSS', isDone: true},
+            {id: crypto.randomUUID(), title: 'JS', isDone: true},
+            {id: crypto.randomUUID(), title: 'REACT', isDone: false},
+            {id: crypto.randomUUID(), title: 'REDUX', isDone: false}
+        ],
+        [todolistId_2]: [
+            {id: crypto.randomUUID(), title: 'HTML2', isDone: true},
+            {id: crypto.randomUUID(), title: 'CSS2', isDone: true},
+            {id: crypto.randomUUID(), title: 'JS2', isDone: true},
+            {id: crypto.randomUUID(), title: 'REACT2', isDone: false},
+            {id: crypto.randomUUID(), title: 'REDUX2', isDone: false}
+        ]
+    })
+
 
     const [filter, setFilter] = useState<FilterType>('all')
 
