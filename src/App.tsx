fb5ce363315130924c4e48a2909c43d66a5a6813
Igor Minayeu <igor.minayeu@gmail.com>
2026-03-09 11:52:48 +0300

    completed lesson 1 repeat

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
 import './App.css'
+import {TaskType, Todolist} from "./Todolist.tsx";
 
 function App() {
-  return (
-      <div className="app">
-        <div>
-          <h3>What to learn</h3>
-          <div>
-            <input/>
-            <button>+</button>
-          </div>
-          <ul>
-            <li>
-              <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={true}/> <span>JS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={false}/> <span>React</span>
-            </li>
-          </ul>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
+    const todolistTitle_1 = "What to learn"
+    const todolistTitle_3 = "What to eat"
+    const tasks_1: Array<TaskType> = [
+        {id: crypto.randomUUID(), title: "HTML", isDone: true},
+        {id: crypto.randomUUID(), title: "CSS", isDone: true},
+        {id: crypto.randomUUID(), title: "JS", isDone: false},
+        {id: crypto.randomUUID(), title: "REACT", isDone: false},
+    ]
+    const tasks_2: Array<TaskType> = [
+        {id: crypto.randomUUID(), title: "BREAD", isDone: true},
+        {id: crypto.randomUUID(), title: "MILK", isDone: true},
+        {id: crypto.randomUUID(), title: "POTATO", isDone: false},
+        {id: crypto.randomUUID(), title: "CHEESE", isDone: false},
+    ]
+    return (
+        <div className="app">
+            <Todolist title={todolistTitle_1} tasks={tasks_1}/>
+            <Todolist title={todolistTitle_3} tasks={tasks_2}/>
         </div>
-      </div>
-  )
+    )
 }
 
 export default App
