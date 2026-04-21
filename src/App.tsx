46ade5a061ddb586c57ee8a0d8f4de30b73b2e2e
Igor Minayeu <igor.minayeu@gmail.com>
2026-04-21 14:33:17 +0300

    add state for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css"
+import {useState} from "react";
 
+export type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
 
 function App() {
+
+    const todolistTitle = 'What to learn'
+    const [tasks, setTasks] = useState<TaskType[]>([
+        {id: crypto.randomUUID(), title: 'HTML', isDone: true},
+        {id: crypto.randomUUID(), title: 'CSS', isDone: true},
+        {id: crypto.randomUUID(), title: 'JS', isDone: false},
+        {id: crypto.randomUUID(), title: 'VITE', isDone: true},
+        {id: crypto.randomUUID(), title: 'REACT', isDone: false},
+        {id: crypto.randomUUID(), title: 'REDUX', isDone: false}
+    ])
 
 
     return (
