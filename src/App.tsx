3fb4e89c6b35ffdf70d9cf0ccc98add6376848c1
Igor Minayeu <igor.minayeu@gmail.com>
2026-05-09 16:43:32 +0300

    add useState for tasks and type for task

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
-import "./App.css"
+import './App.css'
+import {useState} from "react";
+import {TaskType} from "./types/types.ts";
 
 function App() {
+
+    const todolistTitle = 'What to learn'
+    const [tasks, setTasks] = useState<TaskType[]>([
+        {id: crypto.randomUUID(), title: 'HTML', isDone: true},
+        {id: crypto.randomUUID(), title: 'CSS', isDone: true},
+        {id: crypto.randomUUID(), title: 'JS', isDone: true},
+        {id: crypto.randomUUID(), title: 'REACT', isDone: false},
+        {id: crypto.randomUUID(), title: 'RTK', isDone: false},
+        {id: crypto.randomUUID(), title: 'VITE', isDone: true},
+        {id: crypto.randomUUID(), title: 'GIT', isDone: true}
+    ])
 
     return (
         <div className="app">
