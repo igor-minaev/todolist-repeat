dd17db06a1b5c5fa15c01aa0b8dac2bb39ee9d25
Igor Minayeu <igor.minayeu@gmail.com>
2026-06-10 17:08:25 +0300

    add Todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
 import './App.css'
+import {Todolist} from "./Todolist";
 
 function App() {
 
     return (
         <div className="app">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input type="text"/>
-                    <button>x</button>
-                </div>
-                <ul>
-                    <li>
-                        <input type="checkbox" checked/>
-                        <span>HTML</span>
-                    </li>
-                    <li>
-                        <input type="checkbox" checked/>
-                        <span>CSS</span>
-                    </li>
-                    <li>
-                        <input type="checkbox"/>
-                        <span>JS</span>
-                    </li>
-                    <li>
-                        <input type="checkbox"/>
-                        <span>REACT</span>
-                    </li>
-                </ul>
-            </div>
+            <Todolist/>
         </div>
     )
 }
---
+++ src/Todolist.tsx
@@ -0,0 +1,34 @@
+export const Todolist = () => {
+    return (
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input type="text"/>
+                <button>x</button>
+            </div>
+            <ul>
+                <li>
+                    <input type="checkbox" checked/>
+                    <span>HTML</span>
+                </li>
+                <li>
+                    <input type="checkbox" checked/>
+                    <span>CSS</span>
+                </li>
+                <li>
+                    <input type="checkbox"/>
+                    <span>JS</span>
+                </li>
+                <li>
+                    <input type="checkbox"/>
+                    <span>REACT</span>
+                </li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+};
