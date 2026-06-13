b32f6d589e19837c54bcd5916f82b87a4932194d
Igor Minayeu <igor.minayeu@gmail.com>
2026-06-13 13:19:27 +0300

    add Header component

---
+++ src/Heder.tsx
@@ -0,0 +1,43 @@
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import {containerSx} from "@/Todolist.styles";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import {NavButton} from "@/NavButton";
+import Switch from "@mui/material/Switch";
+import AppBar from "@mui/material/AppBar";
+import {useAppSelector} from "@/common/hooks/useAppSelector";
+import {selectThemeMode} from "@/app/app-selectors";
+import {getTheme} from "@/common/theme/theme";
+import {useAppDispatch} from "@/common/hooks/useAppDispatch";
+import {changeThemeModeAC} from "@/app/app-reducer";
+
+export const Header = () => {
+
+    const themeMode = useAppSelector(selectThemeMode)
+    const theme = getTheme(themeMode)
+
+    const dispatch = useAppDispatch()
+
+    const changeThemeMode = () => dispatch(changeThemeModeAC({
+        themeMode: themeMode === 'light' ? 'dark' : 'light'
+    }))
+
+    return (
+        <AppBar position="static" sx={{mb: '30px'}}>
+            <Toolbar>
+                <Container maxWidth='lg' sx={containerSx}>
+                    <IconButton color="inherit">
+                        <MenuIcon/>
+                    </IconButton>
+                    <div>
+                        <NavButton>Sign in</NavButton>
+                        <NavButton>Sign up</NavButton>
+                        <NavButton background={theme.palette.primary.dark}>Faq</NavButton>
+                        <Switch color={'default'} onChange={changeThemeMode}/>
+                    </div>
+                </Container>
+            </Toolbar>
+        </AppBar>
+    )
+}
