ca2cb67e8202e4948360cf759182d5b9ee85ba53
Igor Minayeu <igor.minayeu@gmail.com>
2026-06-28 22:18:59 +0300

    completed lesson 2

--- src/Button.tsx
+++ src/Button.tsx
@@ -3,5 +3,7 @@ import type {ButtonHTMLAttributes} from "react";
 type Props = ButtonHTMLAttributes<HTMLButtonElement>
 
 export const Button = ({children, onClick, ...restProps}: Props) => {
-    return <button onClick={onClick} {...restProps}>{children}</button>
+    return (
+        <button onClick={onClick} {...restProps}>{children}</button>
+    )
 }
