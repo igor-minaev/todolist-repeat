f21e6fd84e040e4135404abf78ba8f65f8a1ec4f
Igor Minayeu <igor.minayeu@gmail.com>
2026-07-01 21:19:24 +0300

    create todolists-reducer

---
+++ src/model/todolists-reducer.test.ts
@@ -0,0 +1,60 @@
+import {beforeEach, expect, test} from 'vitest'
+import type {Todolist} from '../App'
+import {
+    changeTodolistFilterAC,
+    changeTodolistTitleAC,
+    createTodolistAC,
+    deleteTodolistAC,
+    todolistsReducer
+} from './todolists-reducer'
+
+
+let todolistId1: string
+let todolistId2: string
+let startState: Todolist[]
+beforeEach(() => {
+    todolistId1 = crypto.randomUUID()
+    todolistId2 = crypto.randomUUID()
+    startState = [
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'},
+    ]
+})
+
+
+test('correct todolist should be deleted', () => {
+
+    const endState = todolistsReducer(startState, deleteTodolistAC(todolistId1))
+
+    // 3. Проверка, что действие измененило state соответствующим образом
+    // в массиве останется один тудулист
+    expect(endState.length).toBe(1)
+    // удалится нужный тудулист, не любой
+    expect(endState[0].id).toBe(todolistId2)
+})
+
+
+test('correct todolist should be created', () => {
+
+    const title = 'New todolist'
+    const endState = todolistsReducer(startState, createTodolistAC(title))
+
+    expect(endState.length).toBe(3)
+    expect(endState[0].title).toBe(title)
+})
+
+test('correct todolist should change its title', () => {
+    const title = 'New title'
+    const endState = todolistsReducer(startState, changeTodolistTitleAC({todolistId: todolistId2, title}))
+
+    expect(endState[0].title).toBe('What to learn')
+    expect(endState[1].title).toBe(title)
+})
+
+test('correct todolist should change its filter', () => {
+    const filter = 'completed'
+    const endState = todolistsReducer(startState, changeTodolistFilterAC({todolistId: todolistId2, filter}))
+
+    expect(endState[0].filter).toBe('all')
+    expect(endState[1].filter).toBe(filter)
+})
--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -1,22 +1,54 @@
-import type {Todolist} from "@/App";
+import type {FilterValues, Todolist} from "@/App";
+import * as crypto from "node:crypto";
 
 export type DeleteTodolistAT = ReturnType<typeof deleteTodolistAC>
+export type ChangeTodolistFilterAT = ReturnType<typeof changeTodolistFilterAC>
+export type ChangeTodolistTitleAT = ReturnType<typeof changeTodolistTitleAC>
+export type createTodolistAT = ReturnType<typeof createTodolistAC>
 
-type ActionsType = DeleteTodolistAT
+type ActionsType = DeleteTodolistAT | ChangeTodolistFilterAT | ChangeTodolistTitleAT | createTodolistAT
 
 const initialState: Todolist[] = []
 
 export const todolistsReducer = (state: Todolist[] = initialState, action: ActionsType): Todolist[] => {
     switch (action.type) {
         case "delete_todolist":
-
+            return state.filter(todolist => todolist.id !== action.payload.todolistId)
+        case "change_todolist_filter": {
+            const {todolistId, filter} = action.payload
+            return state.map(todolist => todolist.id === todolistId ? {...todolist, filter} : todolist)
+        }
+        case "change_todolist_title": {
+            const {todolistId, title} = action.payload
+            return state.map(todolist => todolist.id === todolistId ? {...todolist, title} : todolist)
+        }
+        case "create_todolist": {
+            const {todolistId, title} = action.payload
+            const newTodolist: Todolist = {id: todolistId, title, filter: 'all'}
+            return [newTodolist, ...state]
+        }
         default:
             return state
     }
 }
 
 
-export const deleteTodolistAC = (id: string) => ({
+export const deleteTodolistAC = (todolistId: string) => ({
     type: 'delete_todolist',
-    payload: {id}
+    payload: {todolistId}
+} as const)
+
+export const changeTodolistFilterAC = (payload: { todolistId: string, filter: FilterValues }) => ({
+    type: 'change_todolist_filter',
+    payload
+} as const)
+
+export const changeTodolistTitleAC = (payload: { todolistId: string, title: string }) => ({
+    type: 'change_todolist_title',
+    payload
+} as const)
+
+export const createTodolistAC = (title: string) => ({
+    type: 'create_todolist',
+    payload: {title, todolistId: crypto.randomUUID()}
 } as const)
